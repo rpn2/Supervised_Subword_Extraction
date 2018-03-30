22b9ed176877bfa687b4368ff5844bc6ff8cb907
Chris Kim <ykim157@illinois.edu>
2018-03-30 09:48:05 -0500

    Fixed engrams

--- engrams.py
+++ engrams.py
@@ -1,40 +1,50 @@
-def create_engram(word, charcount):
-	c = word[charcount]
-	if '<' in c or '>' in c:
-		return ""
-	return word[charcount]
-	
+def create_forward_engram(word, charcount):
+	return word[charcount + 1] if charcount + 2 != len(word) else word[charcount + 1] + ">"
+
+def create_backward_engram(word, charcount):
+	return word[charcount] if charcount != 0 else "<" + word[charcount]
 
 def create_forward_bigram(word, charcount):
-	bigram = word[charcount:charcount+2]
-	if '<' in bigram or '>' in bigram or len(bigram) < 2:
+	bigram = word[charcount+1 : charcount+3]
+	if len(bigram) < 2:
 		return ""
+	if charcount + 3 == len(word):
+		return bigram + ">"
 	return bigram
 
 def create_backward_bigram(word, charcount):
-	bigram = word[charcount:charcount-2:-1]
-	if '<' in bigram or '>' in bigram or len(bigram) < 2:
+	bigram = word[charcount : None if charcount - 2 == -1 else charcount -2  : -1]
+	if len(bigram) < 2:
 	   return ""
+	if charcount - 2 == -1:
+		return bigram + ">"
 	return bigram
 
 def create_forward_trigram(word, charcount):
-	trigram = word[charcount:charcount+3]
-	if('<' in trigram or '>' in trigram or len(trigram) < 3):
+	trigram = word[charcount+1 : charcount+4]
+	if len(trigram) < 3:
 		return ""
+	if charcount + 4 == len(word):
+		return trigram + ">"
 	return trigram
 
 def create_backward_trigram(word, charcount):
-	trigram = word[charcount:charcount-3:-1]
-	if('<' in trigram or '>' in trigram or len(trigram) < 3):
+	trigram = word[charcount: None if charcount - 3 == -1 else charcount -3 : -1]
+	if len(trigram) < 3:
 		return ""
+	if charcount - 3 == -1:
+		return trigram + ">"
 	return trigram
 
 
-#string = "<onetwothreefourfivesix>"
-#for i in range(len(string)):
-#	print("Character is " + string[i])
-#	print("engram: " + create_engrams(string, i))
-#	print("bigram1: " + create_forward_bigrams(string, i))
-#	print("bigram2: " + create_backward_bigrams(string, i))
-#	print("trigram1: " + create_forward_trigrams(string, i))
-#	print("trigram2: " + create_backward_trigrams(string, i))
+string = "onetwothreefourfivesix"
+#for i in range(len(string) - 1):
+#	print("Splitpoint is between " + string[i] + " and " + string[i + 1])
+#	print("____________________________________")
+#	print("engram1: " + create_forward_engram(string, i))
+#	print("engram2: " + create_backward_engram(string, i))
+#	print("bigram1: " + create_forward_bigram(string, i))
+#	print("bigram2: " + create_backward_bigram(string, i))
+#	print("trigram1: " + create_forward_trigram(string, i))
+#	print("trigram2: " + create_backward_trigram(string, i))
+#	print("____________________________________")
